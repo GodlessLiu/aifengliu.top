0c083d5ff726d29e870326b9e3f00f9a1080badb
Hilary Liu <2788370451@qq.com>
2024-05-24 22:03:53 +0800

    feat: add github's img

--- pages/index.vue
+++ pages/index.vue
@@ -11,10 +11,17 @@ useHead({
     }
   ]
 })
+
 </script>
 
 <template>
-  <div class="banner">
-    <article class="markdown-body bg" v-html="$mdRenderer.render(profileText)" />
+  <div class="banner markdown-body bg">
+    <article class="" v-html="$mdRenderer.render(profileText)" />
+    <h4>
+      <p>
+        我的<a href="https://github.com/GodlessLiu" target="_blank">Github</a>情况:
+      </p>
+      <img src="https://ghchart.rshah.org/GodlessLiu" alt="GodlessLiu's Github chart" class="w-full" />
+    </h4>
   </div>
 </template>
